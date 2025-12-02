895a10c7b69132c2e75a09071dab4359e6d3b7cd
xiaoxuan <530141767@qq.com>
2025-12-02 20:57:05 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,504 @@
+<script setup></script>
+
+<template>
+  <div class="todolist">
+    <div class="Left">
+      <div class="Left-body">
+        <div class="Left-header">
+          <svg
+            @click=""
+            t="1764658081387"
+            class="Left-icon"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="4725"
+            width="20"
+            height="20"
+          >
+            <path
+              d="M66.488889 211.781818h891.022222c28.198788 0 50.980202-22.238384 50.980202-49.648485 0-27.397172-22.768485-49.648485-50.980202-49.648485H66.488889C38.341818 112.484848 15.508687 134.723232 15.508687 162.133333s22.833131 49.648485 50.980202 49.648485z m891.009293 248.242424H66.488889C38.277172 460.024242 15.508687 482.262626 15.508687 509.672727s22.768485 49.648485 50.980202 49.648485h891.022222c28.198788 0 50.980202-22.238384 50.980202-49.648485-0.012929-27.410101-22.923636-49.648485-50.993131-49.648485z m0 351.63798H66.488889c-28.134141 0-50.980202 22.238384-50.980202 49.648485s22.833131 49.648485 50.980202 49.648485h891.022222c28.198788 0 50.980202-22.238384 50.980202-49.648485-0.012929-27.397172-22.781414-49.648485-50.993131-49.648485z m0 0"
+              p-id="4726"
+            ></path>
+          </svg>
+        </div>
+        <div class="Left-content">
+          <div class="Left-Listitem">
+            <div class="Left-search">
+              <svg
+                t="1764660333997"
+                class="icon"
+                viewBox="0 0 1024 1024"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                p-id="12429"
+                width="20"
+                height="18"
+              >
+                <path
+                  d="M499.4 837.2c-207.7 0-376.7-169-376.7-376.7s169-376.7 376.7-376.7 376.7 169 376.7 376.7c0 95.5-35.8 186.6-100.8 256.5-71.1 76.3-171.6 120.2-275.9 120.2z m0-690.1C326.6 147.1 186 287.7 186 460.5s140.6 313.4 313.4 313.4c86.7 0 170.4-36.5 229.5-100 54.1-58.2 83.8-133.9 83.8-213.3 0.1-172.9-140.5-313.5-313.3-313.5z"
+                  fill="#666666"
+                  p-id="12430"
+                ></path>
+                <path
+                  d="M891.4 916.5c-8.4 0-16.8-3.3-23.1-10L719 747.9c-12-12.7-11.4-32.8 1.3-44.8 12.8-12 32.8-11.4 44.8 1.3l149.4 158.7c12 12.7 11.4 32.8-1.3 44.8-6.2 5.7-14 8.6-21.8 8.6z"
+                  fill="#666666"
+                  p-id="12431"
+                ></path>
+              </svg>
+              <input class="search" type="text" placeholder="搜索" />
+            </div>
+            <div class="Left-listinner">
+              <svg
+                class="icon"
+                aria-label=""
+                fill="currentColor"
+                aria-hidden="true"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                focusable="false"
+              >
+                <path
+                  d="M10 2c.28 0 .5.22.5.5v1a.5.5 0 01-1 0v-1c0-.28.22-.5.5-.5zm0 12a4 4 0 100-8 4 4 0 000 8zm0-1a3 3 0 110-6 3 3 0 010 6zm7.5-2.5a.5.5 0 000-1h-1a.5.5 0 000 1h1zM10 16c.28 0 .5.22.5.5v1a.5.5 0 01-1 0v-1c0-.28.22-.5.5-.5zm-6.5-5.5a.5.5 0 000-1H2.46a.5.5 0 000 1H3.5zm.65-6.35c.2-.2.5-.2.7 0l1 1a.5.5 0 11-.7.7l-1-1a.5.5 0 010-.7zm.7 11.7a.5.5 0 01-.7-.7l1-1a.5.5 0 01.7.7l-1 1zm11-11.7a.5.5 0 00-.7 0l-1 1a.5.5 0 00.7.7l1-1a.5.5 0 000-.7zm-.7 11.7a.5.5 0 00.7-.7l-1-1a.5.5 0 00-.7.7l1 1z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              <span>今天</span>
+            </div>
+            <div class="Left-listinner">
+              <svg
+                class="icon"
+                aria-label=""
+                fill="currentColor"
+                aria-hidden="true"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                focusable="false"
+              >
+                <path
+                  d="M9.1 2.9a1 1 0 011.8 0l1.93 3.91 4.31.63a1 1 0 01.56 1.7l-3.12 3.05.73 4.3a1 1 0 01-1.45 1.05L10 15.51l-3.86 2.03a1 1 0 01-1.45-1.05l.74-4.3L2.3 9.14a1 1 0 01.56-1.7l4.31-.63L9.1 2.9zm.9.44L8.07 7.25a1 1 0 01-.75.55L3 8.43l3.12 3.04a1 1 0 01.3.89l-.75 4.3 3.87-2.03a1 1 0 01.93 0l3.86 2.03-.74-4.3a1 1 0 01.29-.89L17 8.43l-4.32-.63a1 1 0 01-.75-.55L10 3.35z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              <span>重要</span>
+            </div>
+            <div class="Left-listinner">
+              <svg
+                class="icon"
+                aria-label=""
+                fill="currentColor"
+                aria-hidden="true"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                focusable="false"
+              >
+                <path
+                  d="M9 2.39a1.5 1.5 0 012 0l5.5 4.94c.32.28.5.69.5 1.12v7.05c0 .83-.67 1.5-1.5 1.5H13a1.5 1.5 0 01-1.5-1.5V12a.5.5 0 00-.5-.5H9a.5.5 0 00-.5.5v3.5c0 .83-.67 1.5-1.5 1.5H4.5A1.5 1.5 0 013 15.5V8.45c0-.43.18-.84.5-1.12L9 2.39zm1.33.74a.5.5 0 00-.66 0l-5.5 4.94a.5.5 0 00-.17.38v7.05c0 .28.22.5.5.5H7a.5.5 0 00.5-.5V12c0-.83.67-1.5 1.5-1.5h2c.83 0 1.5.67 1.5 1.5v3.5c0 .28.22.5.5.5h2.5a.5.5 0 00.5-.5V8.45a.5.5 0 00-.17-.38l-5.5-4.94z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              <span>任务</span>
+            </div>
+          </div>
+        </div>
+        <div class="Left-List">
+          <ul class="Left-List-body">
+            <li class="Left-List-box">
+              <svg
+                class="icon"
+                aria-label=""
+                fill="currentColor"
+                aria-hidden="true"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3 6.5a1 1 0 100-2 1 1 0 000 2zm3-1c0-.28.22-.5.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm0 5c0-.28.22-.5.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm.5 4.5a.5.5 0 000 1h11a.5.5 0 000-1h-11zm-2.5.5a1 1 0 11-2 0 1 1 0 012 0zm-1-4a1 1 0 100-2 1 1 0 000 2z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              <span>学习</span>
+              <div class="list-count">0</div>
+            </li>
+            <li class="Left-List-box">
+              <svg
+                class="icon"
+                aria-label=""
+                fill="currentColor"
+                aria-hidden="true"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3 6.5a1 1 0 100-2 1 1 0 000 2zm3-1c0-.28.22-.5.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm0 5c0-.28.22-.5.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm.5 4.5a.5.5 0 000 1h11a.5.5 0 000-1h-11zm-2.5.5a1 1 0 11-2 0 1 1 0 012 0zm-1-4a1 1 0 100-2 1 1 0 000 2z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              <span>学习</span>
+              <div class="list-count">0</div>
+            </li>
+            <li class="Left-List-box">
+              <svg
+                class="icon"
+                aria-label=""
+                fill="currentColor"
+                aria-hidden="true"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3 6.5a1 1 0 100-2 1 1 0 000 2zm3-1c0-.28.22-.5.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm0 5c0-.28.22-.5.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm.5 4.5a.5.5 0 000 1h11a.5.5 0 000-1h-11zm-2.5.5a1 1 0 11-2 0 1 1 0 012 0zm-1-4a1 1 0 100-2 1 1 0 000 2z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              <span>学习</span>
+              <div class="list-count">0</div>
+            </li>
+          </ul>
+        </div>
+        <div class="Left-AddList">
+          <svg
+            class="icon"
+            aria-label=""
+            fill="currentColor"
+            aria-hidden="true"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 2.5a.5.5 0 00-1 0V9H2.5a.5.5 0 000 1H9v6.5a.5.5 0 001 0V10h6.5a.5.5 0 000-1H10V2.5z"
+              fill="currentColor"
+            ></path>
+          </svg>
+          <input type="text" placeholder="添加列表" />
+        </div>
+      </div>
+    </div>
+    <div class="center">
+      <div class="center-header">
+        <svg
+          @click=""
+          t="1764658081387"
+          class="more-icon"
+          viewBox="0 0 1024 1024"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          p-id="4725"
+          width="20"
+          height="20"
+        >
+          <path
+            d="M66.488889 211.781818h891.022222c28.198788 0 50.980202-22.238384 50.980202-49.648485 0-27.397172-22.768485-49.648485-50.980202-49.648485H66.488889C38.341818 112.484848 15.508687 134.723232 15.508687 162.133333s22.833131 49.648485 50.980202 49.648485z m891.009293 248.242424H66.488889C38.277172 460.024242 15.508687 482.262626 15.508687 509.672727s22.768485 49.648485 50.980202 49.648485h891.022222c28.198788 0 50.980202-22.238384 50.980202-49.648485-0.012929-27.410101-22.923636-49.648485-50.993131-49.648485z m0 351.63798H66.488889c-28.134141 0-50.980202 22.238384-50.980202 49.648485s22.833131 49.648485 50.980202 49.648485h891.022222c28.198788 0 50.980202-22.238384 50.980202-49.648485-0.012929-27.397172-22.781414-49.648485-50.993131-49.648485z m0 0"
+            p-id="4726"
+          ></path>
+        </svg>
+        <div class="title">今天</div>
+        <span>•••</span>
+        <div class="no-done">
+          <span class="no-done-count">0</span>
+          <span> 未完成</span>
+        </div>
+      </div>
+      <div class="center-content">
+        <div class="addTask">
+          <svg
+            class="icon"
+            aria-label=""
+            fill="currentColor"
+            aria-hidden="true"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 2.5a.5.5 0 00-1 0V9H2.5a.5.5 0 000 1H9v6.5a.5.5 0 001 0V10h6.5a.5.5 0 000-1H10V2.5z"
+              fill="currentColor"
+            ></path>
+          </svg>
+          <input type="text" placeholder="添加任务" />
+        </div>
+        <div class="center-select-show">
+          <div class="all-task">全部</div>
+          <div class="no-done-task">未完成</div>
+          <div class="done-task">已完成</div>
+        </div>
+        <div class="task">
+          <div class="task-item">
+            <input type="checkbox" />
+            <span>任务1</span>
+            <span class="delect">🗑️</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.todolist {
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  background-color: #f8f8f8;
+}
+
+/* 左边区域 */
+.Left {
+  width: 290px;
+  height: 100%;
+  background-color: #fff;
+}
+
+.Left-body {
+  height: 100%;
+  margin: 0px 10px;
+}
+
+.Left-icon {
+  margin: 28px 0px 0px 10px;
+}
+
+.Left-content {
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+}
+
+.Left-List {
+  max-height: 60%;
+  overflow-y: auto;
+}
+
+.Left-listinner {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  padding: 10px;
+}
+
+.Left-search {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  padding: 10px;
+  color: rgb(100, 100, 241);
+}
+
+.search {
+  outline: none;
+  border: none;
+  font-size: 16px;
+}
+
+.search::placeholder {
+  color: rgb(100, 100, 241);
+}
+
+.search:focus::placeholder {
+  color: #b6b4b4;
+}
+
+.Left-Listitem {
+  padding-bottom: 10px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.267);
+}
+
+.Left-List-body {
+  margin-top: 15px;
+  list-style: none;
+  overflow-y: auto;
+}
+
+.Left-List-box {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  gap: 12px;
+}
+
+.Left-List-box span {
+  width: 75%;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+.list-count {
+  margin-left: auto;
+}
+
+.Left-AddList {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  gap: 12px;
+  color: rgb(100, 100, 241);
+}
+
+.Left-AddList input {
+  border: none;
+  outline: none;
+  font-size: 16px;
+}
+
+.Left-AddList input::placeholder {
+  color: rgb(100, 100, 241);
+}
+
+.Left-AddList input:focus::placeholder {
+  color: #b6b4b4;
+}
+
+/* 选择列表样式 */
+.Selected-list {
+  background-color: #eff6fc;
+}
+
+/* 中间内容区域 */
+.center {
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.center-header {
+  display: flex;
+  gap: 10px;
+  padding: 20px;
+  padding-top: 15px;
+  font-size: 20px;
+  align-items: center;
+}
+
+.title {
+  display: inline;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+.addTask {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  width: 98%;
+  padding: 0px 15px;
+  margin: 0 auto;
+  box-shadow: 0 6px 20px rgba(0, 0, 0, 0.08);
+  color: rgb(100, 100, 241);
+  border-radius: 12px;
+}
+
+.addTask input {
+  width: 100%;
+  height: 8px;
+  padding: 30px 0px;
+  border: none;
+  outline: none;
+  background-color: #f8f8f8;
+  font-size: 16px;
+}
+
+.addTask input::placeholder {
+  color: rgb(100, 100, 241);
+}
+
+.addTask input:focus::placeholder {
+  color: #b6b4b4;
+}
+
+.center-select-show {
+  display: flex;
+  padding: 20px 20px 20px 15px;
+  gap: 10px;
+}
+
+.no-done {
+  font-size: 16px;
+  margin-left: auto;
+  flex: 0 0 100px;
+  text-align: right;
+}
+
+.done-task,
+.no-done,
+.no-done-task,
+.all-task {
+  padding: 10px;
+}
+
+.more-icon {
+  flex: 0 0 auto;
+}
+
+.done-task:hover,
+.no-done-task:hover,
+.all-task:hover {
+  background-color: #73a8f850;
+  border-radius: 5px;
+}
+
+.select-Task-show {
+  background-color: #73a8f850;
+  border-radius: 5px;
+}
+
+.center-content {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  margin-bottom: 30px;
+}
+
+.task {
+  flex: 1;
+  margin: 0px 10px;
+  overflow-y: auto;
+}
+
+.task-item {
+  padding: 20px 20px 20px 20px;
+  display: flex;
+  gap: 10px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.267);
+}
+
+.delect {
+  margin-left: auto;
+}
+
+@media (max-width: 768px) {
+  .Left {
+    display: none;
+  }
+}
+
+@media (max-width: 900px) {
+  .Left {
+    width: 200px;
+  }
+}
+</style>
